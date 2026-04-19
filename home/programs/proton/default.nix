9679b9f7853e026d1f23d9e2fb21d148778209e0
Chiloute <35150997+Chiloute@users.noreply.github.com>
2026-04-19 21:13:28 +0200

    change browser
    Signed-off-by: Chiloute <35150997+Chiloute@users.noreply.github.com>

--- home/programs/proton/default.nix
+++ home/programs/proton/default.nix
@@ -17,7 +17,7 @@
     };
     "Proton Calendar" = {
       name = "Proton Calendar";
-      exec = "${pkgs.qutebrowser}/bin/qutebrowser \"https://calendar.proton.me\"";
+      exec = "zen-beta \"https://calendar.proton.me\"";
       icon = "proton-calendar";
       type = "Application";
       categories = ["Utility"];
@@ -25,7 +25,7 @@
     };
     "Proton Mail" = {
       name = "Proton Mail";
-      exec = "${pkgs.qutebrowser}/bin/qutebrowser \"https://mail.proton.me/\"";
+      exec = "zen-beta \"https://mail.proton.me/\"";
       icon = "proton-mail";
       type = "Application";
       categories = ["Utility"];
