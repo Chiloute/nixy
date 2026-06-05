6b20f99b033e06935b26cd26903dc672edfe08b6
Chiloute <35150997+Chiloute@users.noreply.github.com>
2026-06-05 18:01:05 +0200

    update usbguard config
    Signed-off-by: Chiloute <35150997+Chiloute@users.noreply.github.com>

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -33,6 +33,7 @@
     allow id 1d6b:0002 serial "0000:05:00.3" name "xHCI Host Controller" hash "4a4NgfdUaJO43rkCzmWRSeHHR/uUh5+SNsXnhosm9qs=" parent-hash "ldMchY4Tt4GPUYo30eNGvai+Fs/EdnVY3vMyxJUq4Nk=" with-interface 09:00:00 with-connect-type ""
     allow id 1d6b:0003 serial "0000:05:00.3" name "xHCI Host Controller" hash "d+DNGWARDtv9nEK2ZvnNOCtFernuMu5/e/oZ7kCppqQ=" parent-hash "ldMchY4Tt4GPUYo30eNGvai+Fs/EdnVY3vMyxJUq4Nk=" with-interface 09:00:00 with-connect-type ""
     allow id 0634:5602 serial "2241E32ACBDE" name "CT1000X8SSD9" hash "o9+GOR1Wu6hxCjkYmGMCFpxDXOEHHULG0iA95x/7WS0=" parent-hash "prM+Jby/bFHCn2lNjQdAMbgc6tse3xVx+hZwjOPHSdQ=" via-port "4-1" with-interface { 08:06:50 08:06:62 } with-connect-type "hotplug"
+    allow id 0781:5591 name " SanDisk 3.2Gen1"
   '';
 
   home-manager.users."${config.var.username}" = import ./home.nix;
